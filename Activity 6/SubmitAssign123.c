d8c5e65926b1b6a275496004977da5c65ce19e78
Mickel Angelo Castroverde <mikele.castroberde@gmail.com>
2024-10-03 07:36:44 +0000

    added recent activities

--- Activity 6/SubmitAssign123.c
+++ Activity 6/SubmitAssign123.c
@@ -1,66 +1,65 @@
+/*
+============================================================================
+FILE : EXTRA.c
+AUTHOR : Mickel Angelo Castroverde
+DESCRIPTION : Prints all the pattern of the requested outputs from Ms. Anore
+COPYRIGHT : © 2024 Mickel Angelo Castroverde. All rights reserved.
+REVISION HISTORY
+Date: 10-03-24 By: Mickel Angelo Castroverde Description: finished and refactored the project
+============================================================================
+*/
 #include <stdio.h>
 
 int main() {
-    int limit, i, j, k;
 
-    // Code for Number 1
-    printf("Enter the limit for Number 1: ");
+    int limit, i, j, k;
+    // Prompt for the limit
+    printf("Enter the limit: ");
     scanf("%d", &limit);
 
+    // Check if the limit exceeds 9
     if (limit > 9) {
         printf("Limit Exceeded\n");
-    } else {
-        for (i = 1; i <= limit; i++) {
-            for (j = 1; j < i; j++) {
-                printf("*");
-            }
-            printf("%d\n", i);
+        return 1; // Exit the program if the limit is exceeded
+    }
+
+    // Code for Number 1
+    for (i = 1; i <= limit; i++) {
+        for (j = 1; j < i; j++) {
+            printf("*");
         }
+        printf("%d\n", i);
     }
 
     // Code for Number 2
-    printf("Enter limit for Number 2: ");
-    scanf("%d", &limit);
-
-    if (limit > 9) {
-        printf("Limit Exceeded\n");
-    } else {
-        for (i = 1; i <= limit; i++) {
-            for (j = limit; j > i; j--) {
-                printf(" ");
-            }
-            for (k = 1; k < i; k++) {
-                printf("*");
-            }
-            printf("%d", i);
-            for (k = 1; k < i; k++) {
-                printf("*");
-            }
-            printf("\n");
+    for (i = 1; i <= limit; i++) {
+        for (j = limit; j > i; j--) {
+            printf(" ");
         }
+        for (k = 1; k < i; k++) {
+            printf("*");
+        }
+        printf("%d", i);
+        for (k = 1; k < i; k++) {
+            printf("*");
+        }
+        printf("\n");
     }
 
     // Code for Number 3
-    printf("Enter limit for Number 3: ");
-    scanf("%d", &limit);
-
-    if (limit > 9) {
-        printf("Limit Exceeded\n");
-    } else {
-        for (i = 1; i <= limit; i++) {
-            for (j = 1; j < i; j++) {
-                printf("*");
-            }
-            printf("%d", i);
-            for (k = 1; k <= (limit - i) * 2; k++) {
-                printf(" ");
-            }
-            printf("%d", i);
-            for (j = 1; j < i; j++) {
-                printf("*");
-            }
-            printf("\n");
+    for (i = 1; i <= limit; i++) {
+        for (j = 1; j < i; j++) {
+            printf("*");
         }
+        printf("%d", i);
+        for (k = 1; k <= (limit - i) * 2; k++) {
+            printf(" ");
+        }
+        printf("%d", i);
+        for (j = 1; j < i; j++) {
+            printf("*");
+        }
+        printf("\n");
     }
 
     return 0;
